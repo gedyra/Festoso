bda3dbfa64d62b066af9400b31b69d54ec66b665
gedyra <gedyra@gmail.com>
2018-11-12 19:42:24 +0900

    動画表示を新しいロジックに対応した

--- webroot/import_media.php
+++ webroot/import_media.php
@@ -14,25 +14,62 @@ session_start();
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 require "$root/mission6/database.php";
 
-if (isset($_GET["target"]) && $_GET["target"] !== "") {
-    $target = $_GET["target"];
-} else {
-    header("Location: submit_movie.php");
+$target = filter_input(INPUT_GET, 'target');
+if ($target === "") {
+    header('HTTP1.0/404 404 Not Found');
 }
+
 $MIMETypes = array(
     'mp4' => 'video/mp4'
 );
 
 try {
     $pdo = connect();
-    $sql = 'SELECT * FROM movie WHERE title_hash=?';
+    $sql = 'SELECT * FROM Movie INNER JOIN User ON Movie.user_id = User.id WHERE path=?';
     $stmt = $pdo->prepare($sql);
     $param[] = $target;
     $stmt->execute($param);
 
     $raw = $stmt->fetch(PDO::FETCH_ASSOC);
-    header('Content-Type: ' . $MIMETypes['mp4']);
-    echo($raw["row_data"]);
 } catch (PDOException $e) {
     exit($e->getMessage());
 }
+?>
+
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>動画</title>
+</head>
+<body>
+
+<h3><? echo $raw['music_title'] ?></h3>
+<video src="<? echo $target ?>" type="video/mp4" controls height="240" width="320"></video>
+
+<table>
+    <tr>
+        <td>作曲者情報</td>
+        <td>
+            <? if ($raw['composer'] !== '') {
+                echo $raw['composer'];
+            } else {
+                echo 'まだ登録されていません';
+            }
+            ?>
+        </td>
+    </tr>
+    <tr>
+        <td>演奏者情報</td>
+        <td>
+            <a href="profile.php?id=<? echo $raw['user_id'] ?>">
+                <? echo $raw['group_name'] ?>
+            </a>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
--- webroot/submit_movie.php
+++ webroot/submit_movie.php
@@ -31,9 +31,9 @@ function upload_movie()
         switch ($_FILES['movie']['error']) {
             case UPLOAD_ERR_OK:
                 $tmp_name = $_FILES['movie']['tmp_name'];
-                $basename = basename($_FILES['image']['name']);
+                $basename = basename($_FILES['movie']['name']);
                 $name = mb_convert_encoding($basename, 'UTF-8', 'AUTO');
-                $path = "../movies/$name";
+                $path = '../movies/' . hash('sha256', $name);
                 move_uploaded_file($tmp_name, $path);
 
                 return array('success' => true, 'name' => $name, 'path' => $path, 'error' => $error);
@@ -66,25 +66,28 @@ if (isset($_SESSION['login_user'])) {
             }
         } else {
 
+            $music_title = filter_input(INPUT_POST, 'music_title');
+            $composer = filter_input(INPUT_POST, 'composer');
             // DBに格納するファイルネーム設定
             // サーバ側の一時的なファイルネームと取得時刻を結合した文字列にsha256をかける
+            var_dump($music_title);
 
             $stmt = $pdo->prepare(
-                'INSERT INTO Movie(id, title, path, user_id)'
-                . 'VALUES (null, :title, :path, :user_id)'
+                'INSERT INTO Movie(id, title, music_title, composer, path, user_id)'
+                . 'VALUES (null, :title, :music_title, :composer, :path, :user_id)'
             );
             $stmt->bindParam(':title', $movie_info['name']);
+            $stmt->bindParam(':music_title', $music_title);
+            $stmt->bindParam(':composer', $composer);
             $stmt->bindParam(':path', $movie_info['path']);
             $stmt->bindParam(':user_id', $login_user['id']);
             $stmt->execute();
 
-            echo 'アップロードしました';
-
             //header('Location:profile.php?id=' . $login_user['id'] . '&movie=true');
         }
-    } else {
-        header('Location:login.php');
     }
+} else {
+    header('Location:login.php');
 }
 ?>
 
@@ -99,12 +102,23 @@ if (isset($_SESSION['login_user'])) {
 </head>
 <body>
 <?php if ($state_login === true): ?>
-    <?php foreach ($err as $e): ?>
-        <p class="error"><?php echo h($e) ?></p>
-    <?php endforeach; ?>
+    <!--    --><?php //foreach ($err as $e): ?>
+    <!--        <p class="error">--><?php //echo h($e)?><!--</p>-->
+    <!--    --><?php //endforeach; ?>
+    <h2>動画アップロード</h2>
     <form action="submit_movie.php" enctype="multipart/form-data" method="post">
-        <input type="file" name="movie" accept="video/*">
-        <input type="submit" value="アップロード">
+        <p>
+            <label for="music_title">曲名</label>
+            <input type="text" id="music_title" name="music_title">
+        </p>
+        <p>
+            <label for="composer">作曲者名</label>
+            <input type="text" id="composer" name="composer">
+        </p>
+        <p>
+            <input type="file" name="movie" accept="video/*">
+            <input type="submit" value="アップロード">
+        </p>
     </form>
 <?php else: ?>
     <?php header('Location:login.php'); ?>
